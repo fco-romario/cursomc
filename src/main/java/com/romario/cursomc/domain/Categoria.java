e0ca89468ed4fef3abc1da5b3d0d6ea7f2475dd9
Francisco Romario Alves de Lima <romario.lima@saudece.ce.gov.br>
2024-01-11 16:01:41 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/romario/cursomc/domain/Categoria.java
+++ src/main/java/com/romario/cursomc/domain/Categoria.java
@@ -1,23 +1,31 @@
 package com.romario.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable{
-	
-	
+public class Categoria implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY )
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	@JsonIgnore
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -44,6 +52,14 @@ public class Categoria implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -61,6 +77,5 @@ public class Categoria implements Serializable{
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
-	
 	
 }
