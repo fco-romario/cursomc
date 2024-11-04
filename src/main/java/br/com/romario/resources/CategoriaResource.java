18ebed1e82330a13db0f964baa67aa1a0c9d881a
fco-romario <fco.romario.alves@gmail.com>
2024-11-03 21:34:42 -0300

    Produto e associação muitos para muitos

--- src/main/java/br/com/romario/resources/CategoriaResource.java
+++ src/main/java/br/com/romario/resources/CategoriaResource.java
@@ -1,15 +1,10 @@
 package br.com.romario.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.romario.domain.Categoria;
